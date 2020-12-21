c2e9cc8f8a53198e6c64ea97b2b8c44f12d3165c
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2020-12-21 23:27:18 +0000

    Done.

--- greeting.rb
+++ greeting.rb
@@ -8,4 +8,12 @@ def say_greeting_five_times
   end
 end
 
-say_greeting_five_times
+def greeting_a_person(name)
+  puts "Hello #{name}"
+end
+
+def greeting_programmer(name, language)
+  puts "Hello #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting_programmer("Evan", "Javascript")
